2af8b43cedfe6f22718f5a3e239ed3cc1da49c2c
ItzCrazyKns <kushagra20103@gmail.com>
2023-01-22 13:16:37 +0530

    NodeJs 18 support

---
+++ node_modules/topojson-simplify/src/simplify.js
@@ -0,0 +1,29 @@
+export default function(topology, minWeight) {
+  minWeight = minWeight == null ? Number.MIN_VALUE : +minWeight;
+
+  // Remove points whose weight is less than the minimum weight.
+  var arcs = topology.arcs.map(function(input) {
+    var i = -1,
+        j = 0,
+        n = input.length,
+        output = new Array(n), // pessimistic
+        point;
+
+    while (++i < n) {
+      if ((point = input[i])[2] >= minWeight) {
+        output[j++] = [point[0], point[1]];
+      }
+    }
+
+    output.length = j;
+    return output;
+  });
+
+  return {
+    type: "Topology",
+    transform: topology.transform,
+    bbox: topology.bbox,
+    objects: topology.objects,
+    arcs: arcs
+  };
+}
